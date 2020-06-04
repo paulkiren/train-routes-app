92cf2542073d577f165704f5ca6c81d9810151a7
PaulKiren <kirenpaul@gmail.com>
2020-06-05 02:05:32 +0700

    Logs added

--- src/index.js
+++ src/index.js
@@ -2,44 +2,26 @@ var Graph = require('./Graph');
 var CSVParser = require('./CSVParser');
 const prompt = require('prompt');
 const Logger = require('./Logger');
-Logger.info(process.argv);
-var fileName = process.argv[2]? process.argv[2].split('=')[1]: '';
+var fileName = process.argv[2] ? process.argv[2].split('=')[1] : '';
 (function () {
-
-    console.log(arguments);
-    let file= arguments[0]|| null;
+    let file = arguments[0] || null;
     const csvParser = new CSVParser(file);
     csvParser.readfile().then(csvData => {
         let map = new Graph(csvData);
-
-        const properties = [
-            {
-                name: 'StartStation',
-                validator: /^[a-zA-Z\s\-]+$/,
-                warning: 'Username must be only letters, spaces, or dashes'
-            },
-            {
-                name: 'EndStation',
-                hidden: false
-            }
-        ];
-
+        const properties = [{ name: 'StartStation' }, { name: 'EndStation' }];
         prompt.start();
 
         prompt.get(properties, function (err, result) {
             if (err) { return onErr(err); }
 
-            console.log(map.findRoute(result.StartStation, result.EndStation));
-            // console.log('Command-line input received:');
-            // console.log('  Start_Station: ' + result.StartStation);
-            // console.log('  End_Station: ' + result.EndStation);
+           Logger.info(map.findRoute(result.StartStation, result.EndStation));
         });
 
         function onErr(err) {
-            console.log(err);
+            Logger.error(err);
             return 1;
         }
 
-    }).catch(err => console.log(err));
+    }).catch(err => Logger.error(err));
 
 })(fileName)
